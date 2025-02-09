0ab7dbdad8d529af1e06f9c7ae90dbd85a45a9d8
devoloper mickey <arpanbera475@gmail.com>
2025-02-09 11:42:00 +0530

    navbar added!

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className=' bg-sky-900 h-screen text-white text-2xl flex justify-center items-center'>
-      hello
-    </div>
+    <Navbar/>
   )
 }
 
