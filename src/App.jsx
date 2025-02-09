894762a3a836dca0b82f7bca70d5a4ec78b7fc07
4F24L <afzalmir2003@gmail.com>
2025-02-09 11:41:51 +0530

    pages added home page and dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Dashboard from './pages/Dashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState('');
 
   return (
     <div className=' bg-sky-900 h-screen text-white text-2xl flex justify-center items-center'>
-      hello
+      <Dashboard/>
     </div>
   )
 }
